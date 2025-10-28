d32f88c60fcb8928ec979813b5b81218d9be2212
tranhuulocnapa <tranhuulocnapa@gmail.com>
2025-10-29 01:25:13 +0800

    update souce version 1

---
+++ src/BaiTapThucHanhLayout/body/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './Banner'
+import Item from './Item'
+
+function Body() {
+    return (
+        <>
+            <Banner />
+            <Item />
+        </>
+    )
+}
+
+export default Body
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Header from './BaiTapThucHanhLayout/Header'
+import Body from './BaiTapThucHanhLayout/body/body'
+import Footer from './BaiTapThucHanhLayout/footer'
+
+createRoot(document.getElementById('root')).render(
+
+
+
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
+
+
+
+
+
+
+
+)
